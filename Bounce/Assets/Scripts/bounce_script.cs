9245e4b916479b4a97ad275c5b8d9c200dd19508
Nick Lewis <lewisn2@rpi.edu>
2014-03-26 17:31:28 -0400

    physics, hard mode, and more!

--- Bounce/Assets/Scripts/bounce_script.cs
+++ Bounce/Assets/Scripts/bounce_script.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class bounce_script : MonoBehaviour {
 	
+	public float bounceForce;
 	// Use this for initialization
 	void Start () {
-	
+		bounceForce = 2000;
 	}
 	void OnCollisionEnter2D(Collision2D thing)
 	{
-		if(thing.transform.name != "floor") 
-			rigidbody2D.AddForce(new Vector2(0,1750));
-		//if (thing.transform.name == "floor")
-		//	game.isGameOver = true;
-		//Debug.Log (thing.transform.name);
+		bounceForce = 35000 / thing.transform.localScale.x;
+		if(thing.transform.name != "floor") // && thing.transform.name != "left_wall" && thing.transform.name != "right_wall") 
+		{
+			Vector3 temp = thing.contacts[0].normal;
+			if (thing.transform.name == "left_wall" || thing.transform.name == "right_wall") 
+				print (temp);
+			//temp*= 2000;
+			//if (temp.magnitude >= 2000)
+			//{
+				temp = temp.normalized;
+				temp = temp*bounceForce;
+				rigidbody2D.AddForce(new Vector2(temp.x,temp.y));
+			//}
+			//else
+			//	rigidbody2D.AddForce(new Vector2(temp.x,temp.y));
+
+		}
 	}
 	// Update is called once per frame
 	void Update () {

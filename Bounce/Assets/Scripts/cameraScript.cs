18e8acd6765c9531928a69ef9e3d5ad8bc3f88f6
Ashley <ashtanski@gmail.com>
2014-04-09 22:05:16 -0400

    camera script1

--- Bounce/Assets/Scripts/cameraScript.cs
+++ Bounce/Assets/Scripts/cameraScript.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class cameraScript : MonoBehaviour {
 
 	public WebCamTexture cameraFront, cameraBack;
+	public WebCamTexture finalCamTexture;
 	public GUITexture BackgroundTexture;
 	public string frontCamName, backCamName;
 	public GameObject picturePlane;
 	public bool isFront;
 	public Vector3 frontRotation, backRotation, frontScale, backScale;
+	public Texture finalPicture;
 
 	// Use this for initialization
 	void Start () {
@@ -54,10 +57,46 @@ public class cameraScript : MonoBehaviour {
 	void Update () {
 
 	}
+	/*
+	void TakeSnapshot()
+	{
+		Texture2D snap = new Texture2D(wct.width,wct.height);
+		snap.SetPixels(wct.GetPixels());
+		snap.Apply();
+		byte[] bytes = snap.EncodeToPNG();
+		string filename = fileName(Convert.ToInt32(snap.width),Convert.ToInt32(snap.height));
+		path = Application.persistentDataPath + "/Snapshots/" + filename;
+		System.IO.File.WriteAllBytes(path,bytes);
+	}*/
+	
+	void SaveTextureToFile( Texture2D text, string fileName)
+	{
+		byte[] bytes = text.EncodeToPNG();
+		//File file2 = new File();
+		FileStream file2 = File.Open(Application.dataPath + "/Resources/"+fileName,FileMode.Create);
+		BinaryWriter binary= new BinaryWriter(file2);
+		binary.Write(bytes);
+		file2.Close();
+	}
 
 	void OnGUI(){
 		//Stops the picture
 		if(GUI.Button(new Rect(0.0f, (Screen.height / 4.0f) * 3.0f, Screen.width, Screen.height / 4), "Tap Here to take Picture")){
+			if (isFront)
+				finalCamTexture = cameraFront;
+			else
+				finalCamTexture = cameraBack;
+			// Ashley messing with camera stuff
+			//finalPicture = picturePlane.renderer.material.mainTexture;
+			Texture2D text = new Texture2D(finalCamTexture.width, finalCamTexture.height, TextureFormat.ARGB32, false);
+			Color[] textureData = finalCamTexture.GetPixels();
+
+			text.SetPixels(textureData);
+			text.Apply();
+			//Texture text = picturePlane.renderer.material.mainTexture;
+			string fileName = "testIMAGE.png";
+			SaveTextureToFile(text, fileName);
+			//Ashley done messing with camera stuff
 			cameraFront.Stop ();
 			cameraBack.Stop ();
 			GetComponent<GameController>().facePicked = true;
@@ -70,6 +109,7 @@ public class cameraScript : MonoBehaviour {
 				cameraFront.Stop();
 				cameraBack.Play ();
 				picturePlane.renderer.material.mainTexture = cameraBack;
+
 				//picturePlane.transform.eulerAngles = backRotation;
 				//picturePlane.transform.localScale = backScale;
 				isFront = false;
@@ -77,6 +117,7 @@ public class cameraScript : MonoBehaviour {
 				cameraBack.Stop ();
 				cameraFront.Play ();
 				picturePlane.renderer.material.mainTexture = cameraFront;
+
 				//picturePlane.transform.eulerAngles = frontRotation;
 				//picturePlane.transform.localScale = frontScale;
 				isFront = true;

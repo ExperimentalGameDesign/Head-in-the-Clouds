70f7405c4f472071a09f32b7cc40136297787524
Nicholas Cesare <callofdutycesare@aol.com>
2014-04-15 16:31:33 -0400

    Instructions Button. Need some art from Anthony for Instructions Screen

--- Bounce/Assets/Scripts/SplashScreen.cs
+++ Bounce/Assets/Scripts/SplashScreen.cs
@@ -5,20 +5,20 @@ public class SplashScreen : MonoBehaviour {
 
 
 	public GameObject splashSprite, faceSprite;
-	private GameObject temp, temp2;
+	private GameObject startButton, instructionsButton;
 	private float fadeValue = 1.0f;
 	private float currentTime = 0.0f;
 	private const float timeItTakesToFade = 0.5f;
 	private bool isFading = false;
+	private bool didInstructions;
 	//private SelectScreen selecter;
 //	private bool isAppearing = false;
 
 	// Use this for initialization
 	void Start () {
-		temp = GameObject.Find ("StartButton");
-		//temp.transform.localScale = new Vector3 (5.939844f, 5.939858f, 0.0f);
-		//temp2 = GameObject.Instantiate (selectSprite, new Vector3 (10.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
-		//temp2.transform.localScale = new Vector3 (10.0f, 19.0f, 0.0f);
+		didInstructions = false;
+		startButton = GameObject.Find ("StartButton");
+		instructionsButton = GameObject.Find ("InstructionsButton");
 	}
 	
 	// Update is called once per frame
@@ -28,12 +28,21 @@ public class SplashScreen : MonoBehaviour {
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 			if (hit.collider != null) {
-				if(hit.collider.name == temp.name) {
-					GameObject.Destroy(splashSprite);
+				if(hit.collider.name == startButton.name) {		//Player touched the Start Button
+					GameObject.Destroy (startButton);
+
+					if(!didInstructions){
+						GameObject.Destroy(splashSprite);
+						GameObject.Destroy (instructionsButton);
+						didInstructions = true;
+					}
 					GetComponent<SelectScreen>().enabled = true;
-					//selectSprite.GetComponent<SpriteRenderer>().enabled = true;
-					//faceSprite.GetComponent<SpriteRenderer>().enabled = true;
 					enabled = false;
+				}else if(hit.collider.name == instructionsButton.name){	//Player touched the Instructions Button
+					Debug.Log ("instructions");
+					GameObject.Destroy (splashSprite);
+					GameObject.Destroy (instructionsButton);
+
 				}
 			}
 		}

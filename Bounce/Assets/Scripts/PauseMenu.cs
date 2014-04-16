a24e8357722f77bc2f2e0bc6a9e9d9910572936a
Nick Lewis <lewisn2@rpi.edu>
2014-04-16 18:01:44 -0400

    pause buttons

--- Bounce/Assets/Scripts/PauseMenu.cs
+++ Bounce/Assets/Scripts/PauseMenu.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class PauseMenu : MonoBehaviour {
 
-	public GameObject pauseSprite;
+	public GameObject pauseSprite, retrySprite, newBallSprite, quitSprite;
 	public bool isPaused = false;
 	// Use this for initialization
 	void Start () {
-		pauseSprite.transform.position = new Vector3 ( 28, 45, 0.0f);//Camera.main.ScreenToWorldPoint(new Vector3 (70*resx, 15*resy, 10.0f));
+		pauseSprite.transform.position = new Vector3 ( 26, 45, 0.0f);//Camera.main.ScreenToWorldPoint(new Vector3 (70*resx, 15*resy, 10.0f));
 		pauseSprite.GetComponent<SpriteRenderer> ().enabled = true;
 		pauseSprite.GetComponent<BoxCollider2D> ().enabled = true;
 	}
@@ -29,10 +29,27 @@ public class PauseMenu : MonoBehaviour {
 			if (isPaused) {
 				Time.timeScale = 0.0f;
 				Camera.main.GetComponent<draw>().enabled = false;
+				retrySprite.GetComponent<SpriteRenderer> ().enabled = true;
+				retrySprite.GetComponent<BoxCollider2D> ().enabled = true;
+
+				newBallSprite.GetComponent<SpriteRenderer> ().enabled = true;
+				newBallSprite.GetComponent<BoxCollider2D> ().enabled = true;
+
+				quitSprite.GetComponent<SpriteRenderer> ().enabled = true;
+				quitSprite.GetComponent<BoxCollider2D> ().enabled = true;
+
 			}
 			if (isPaused == false) {
 				Time.timeScale = 1.0f;
 				Camera.main.GetComponent<draw>().enabled = true;
+				retrySprite.GetComponent<SpriteRenderer> ().enabled = false;
+				retrySprite.GetComponent<BoxCollider2D> ().enabled = false;
+				
+				newBallSprite.GetComponent<SpriteRenderer> ().enabled = false;
+				newBallSprite.GetComponent<BoxCollider2D> ().enabled = false;
+				
+				quitSprite.GetComponent<SpriteRenderer> ().enabled = false;
+				quitSprite.GetComponent<BoxCollider2D> ().enabled = false;
 			}
 		}
 	}
